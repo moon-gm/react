352dc942c4410426c38ed332cab1534b55f2339d
akinaga <akinaga@freedomland.jp>
2020-06-09 12:41:32 +0900

    デプロイ時設定修正

---
+++ docs/precache-manifest.f34c2a0a746bf0ee5c37cee4ad50f767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecb83cbb0d685a8cf4faf81103dc17a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39965bb8c33a9b0eb4dd",
+    "url": "./static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "bfeb7fcd39107e61f32e",
+    "url": "./static/js/2.92667ead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.92667ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39965bb8c33a9b0eb4dd",
+    "url": "./static/js/main.81425636.chunk.js"
+  },
+  {
+    "revision": "f89a2b153ce88117bd68",
+    "url": "./static/js/runtime-main.379af782.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c73b18fc20c655b397f794022d97a8ce.js"
+  "./precache-manifest.f34c2a0a746bf0ee5c37cee4ad50f767.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
