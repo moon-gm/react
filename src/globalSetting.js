4070fba8e39ee0b7b60daeccbc6c8b42037a09ab
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-06-15 13:42:52 +0900

    グローバル設定追加

---
+++ src/globalSetting.js
@@ -0,0 +1,11 @@
+/** CSSのグローバル設定 **/
+// リセット設定
+import './sass/reset.scss';
+// 変数設定
+import './sass/variable.scss';
+// 初期設定
+import './sass/init.scss';
+// レイアウト設定
+import './sass/layout.scss';
+// 共通設定
+import './sass/common.scss';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './sass/index.scss';
-import Router from './component/sample/router';
+import Router from './component/router';
 import * as serviceWorker from './serviceWorker';
+import './globalSetting';
 
 ReactDOM.render(
   <React.StrictMode>
