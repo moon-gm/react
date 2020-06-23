c7c4bec49461c1e1e762a5d34e9ca8561befc85a
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-06-23 18:34:28 +0900

    トップ画面修正

--- src/view/pages/top.js
+++ src/view/pages/top.js
@@ -5,25 +5,22 @@ import Styles from './../../sass/pages/top.module.scss';
 const Top = ({name}) => {
 
 	return (
-		<div className={Styles.App}>
-			<div className={Styles.AppHeader}>
-				<p className={Styles.mainTitle}>
-					{name}
-				</p>
-				<img
-					src={logo}
-					className={Styles.AppLogo}
-					alt="logo"
-				/>
-				<a
-					className={Styles.AppLink}
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					> Go to Support
-				</a>
-			</div>
+		<div className={Styles.AppHeader}>
+			<p className={Styles.mainTitle}>
+				{name}
+			</p>
+			<img
+				src={logo}
+				className={Styles.AppLogo}
+				alt="logo"
+			/>
+			<a
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				> Go to Support
+			</a>
 		</div>
 	);
 }
