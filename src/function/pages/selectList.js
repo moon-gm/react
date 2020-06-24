01b988558b32bfac9082fd4a569e507e55aeaa0f
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-06-24 18:54:55 +0900

    リスト押下時に子リストを非表示にできる処理追加

--- src/function/pages/selectList.js
+++ src/function/pages/selectList.js
@@ -1,6 +1,12 @@
+import States from "./../../config/states";
+
 // リスト押下時の子リストの表示処理
 function selectList(listState) {
-	this.setState({list: listState});
+	if (this.state.list === listState) {
+		this.setState({list: States.list.hide});
+	} else {
+		this.setState({list: listState});
+	}
 }
 
 export default selectList;
