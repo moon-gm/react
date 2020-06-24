98be86f75f3734ba458aee92918b95f84061f143
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-06-24 16:48:27 +0900

    ヘッダータイトルの設定を自動化

--- src/view/routes/header.js
+++ src/view/routes/header.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import States from "../../config/states";
 
+// ヘッダータイトルのルーティング設定
 const HeaderRouter = ({allData, states}) => {
-
 	return (
 		<>
-			{/* Topページ */}
-			{states.page === States.page.Top && allData[0].name}
+			{ // 全ページ設定情報（allData）をループして表示するヘッダータイトルを設定
+				allData.map(pageData => {
+					// pageDataに子リスト(children)がない場合のルーティング
+					if (pageData.children === void 0) {
+						return (
+							<React.Fragment key={`page${pageData.state.page}`}>
+								{states.page === pageData.state.page && pageData.name}
+							</React.Fragment>
+						);
+					}
 
-			{/* 1. 環境構築ページ */}
-			{/* Section1 */}
-			{states.page === States.page.Page1.S1 && allData[1].children[0].name }
-			{/* Section2 */}
-			{states.page === States.page.Page1.S2 && allData[1].children[1].name }
-
-			{/* 2. データ関連ページ */}
-			{/* Section1 */}
-			{states.page === States.page.Page2.S1 && allData[2].children[0].name}
-			{/* Section2 */}
-			{states.page === States.page.Page2.S2 && allData[2].children[1].name}
-			{/* Section3 */}
-			{states.page === States.page.Page2.S3 && allData[2].children[2].name}
+					// pageDataに子リスト(children)がある場合のルーティング
+					else {
+						return (
+							<React.Fragment key={`page${pageData.state.page}`}>
+								{ // 子リスト(children)がある個別ページ設定情報（pageData）をループして表示するヘッダータイトルを設定
+									pageData.children.map(pageDataWithC => {
+										return (
+											<React.Fragment key={`page${pageDataWithC.state.page}`}>
+												{states.page === pageDataWithC.state.page && pageDataWithC.name}
+											</React.Fragment>
+										);
+									})
+								}
+							</React.Fragment>
+						);
+					}
+				})
+			}
 		</>
 	);
 }
