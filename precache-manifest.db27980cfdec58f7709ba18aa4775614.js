0093e835a8f638f4f7cec2e927c4eeb9d6df155a
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-07-14 20:37:55 +0900

    Updates

---
+++ precache-manifest.db27980cfdec58f7709ba18aa4775614.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b387b1a444686a4d69fb01717b648ad5",
+    "url": "/react-learning/index.html"
+  },
+  {
+    "revision": "07ecd571e47a01000226",
+    "url": "/react-learning/static/css/main.d2d9e3f1.chunk.css"
+  },
+  {
+    "revision": "bc3133e6ba94f5cd8305",
+    "url": "/react-learning/static/js/2.ec210740.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-learning/static/js/2.ec210740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ecd571e47a01000226",
+    "url": "/react-learning/static/js/main.2b2f4311.chunk.js"
+  },
+  {
+    "revision": "af8c964980f4b6d1c2a8",
+    "url": "/react-learning/static/js/runtime-main.87629948.js"
+  },
+  {
+    "revision": "f1ad1539accf5f475689908a15801c3e",
+    "url": "/react-learning/static/media/environment_react-start.f1ad1539.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-learning/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c4818ac3d833580f77ce1944a78f540f.js"
+  "/react-learning/precache-manifest.db27980cfdec58f7709ba18aa4775614.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-learning/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
